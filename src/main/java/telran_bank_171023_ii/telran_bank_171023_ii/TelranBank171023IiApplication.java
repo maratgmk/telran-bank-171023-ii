716b4ce5871636f67149b084e0d5f7416dd15d1e
Marat Gafiev <gmkorg109@gmail.com>
2024-05-25 15:20:33 +0300

    Initial commit

---
+++ src/main/java/telran_bank_171023_ii/telran_bank_171023_ii/TelranBank171023IiApplication.java
@@ -0,0 +1,13 @@
+package telran_bank_171023_ii.telran_bank_171023_ii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelranBank171023IiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelranBank171023IiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran_bank_171023_ii/telran_bank_171023_ii/TelranBank171023IiApplicationTests.java
@@ -0,0 +1,13 @@
+package telran_bank_171023_ii.telran_bank_171023_ii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelranBank171023IiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
